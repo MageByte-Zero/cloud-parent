7dcc12642d84b4c1be182fc1f26536d36467574e
jianqing.li <jianqing.li@jfz.com>
2018-05-10 19:07:19 +0800

    zuul gateway初始化

---
+++ api-gateway/src/main/java/com/zero/gateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.zero.gateway;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author jianqing.li
+ * @date 2018/5/10
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ApiGatewayApplication.class).run(args);
+    }
+}
