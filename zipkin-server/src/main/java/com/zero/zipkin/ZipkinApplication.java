64fdd59ca0c79010cb41eef8e0ac426607a8089e
jianqing.li <jianqing.li@jfz.com>
2018-05-29 14:52:04 +0800

    cloud sleuth zipkin http整合方式pom依赖修改

--- zipkin-server/src/main/java/com/zero/zipkin/ZipkinApplication.java
+++ zipkin-server/src/main/java/com/zero/zipkin/ZipkinApplication.java
@@ -2,7 +2,6 @@ package com.zero.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 
 /**
@@ -10,7 +9,6 @@ import zipkin.server.EnableZipkinServer;
  * @EnableZipkinServer注解来启动Zipkin Server
  * @date 2018/5/28
  */
-@EnableEurekaClient
 @EnableZipkinServer
 @SpringBootApplication
 public class ZipkinApplication {
