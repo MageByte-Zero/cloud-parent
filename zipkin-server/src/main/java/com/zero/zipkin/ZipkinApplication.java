67246bc3fa8135658d8d1f24a0a1a25c818e9836
jianqing.li <jianqing.li@jfz.com>
2018-05-29 17:47:25 +0800

    cloud sleuth rabbitMQ收集sleuth调用链跟踪信息到zipkin

--- zipkin-server/src/main/java/com/zero/zipkin/ZipkinApplication.java
+++ zipkin-server/src/main/java/com/zero/zipkin/ZipkinApplication.java
@@ -6,10 +6,12 @@ import zipkin.server.EnableZipkinServer;
 
 /**
  * @author jianqing.li
- * @EnableZipkinServer注解来启动Zipkin Server
+ * @EnableZipkinServer注解来启动Zipkin Server http方式
+ * @EnableZipkinStreamServer 中间件方式
  * @date 2018/5/28
  */
 @EnableZipkinServer
+//@EnableZipkinStreamServer
 @SpringBootApplication
 public class ZipkinApplication {
     public static void main(String[] args) {
